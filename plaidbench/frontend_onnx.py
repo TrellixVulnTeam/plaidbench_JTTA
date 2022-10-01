e7bab35e836d4302d0f4843af2845fe09d75f229
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 05:54:18 +0000

    Adding tarfile member sanitization to extractall()

--- plaidbench/frontend_onnx.py
+++ plaidbench/frontend_onnx.py
@@ -77,7 +77,26 @@ def download_onnx_data(model, filename, use_cached_data=True):
         click.echo('Extracting {}...'.format(compressed_file), nl=False)
         try:
             with tarfile.open(compressed_file) as f:
-                f.extractall(onnx_models)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(f, onnx_models)
         except:
             click.echo("Failed to extract data {} for {}".format(filename, model))
             raise
