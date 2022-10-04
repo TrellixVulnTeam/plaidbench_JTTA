91cda2d3ec0ca8f74e2f0e7558a1d8927ba2e7ed
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 03:12:18 +0000

    Adding numeric_owner as keyword arguement

--- plaidbench/frontend_onnx.py
+++ plaidbench/frontend_onnx.py
@@ -93,7 +93,7 @@ def download_onnx_data(model, filename, use_cached_data=True):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(f, onnx_models)
